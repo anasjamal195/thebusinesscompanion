b043280df139955f27be5ddbcc675c8c7c1bfc8a
anas <anasj5749@gmail.com>
2026-06-17 19:19:21 +0500

    Landing page for goalchaser.co

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Sign up · dialer.best</title>
+        <title>Sign up · GoalChaser.co</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="bg-gray-50 text-gray-900 antialiased">
